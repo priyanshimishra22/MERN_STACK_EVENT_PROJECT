83e7bea26e44889474f907de58ec938a82a0eb7c
Priyanshi Mishra <priyanshi0322@gmail.com>
2024-05-10 18:52:56 +0530

    first commit

---
+++ FRONTEND/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>Lorem ipsum, Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore optio id, corporis nostrum nihil at possimus, repellat explicabo earum quis quod architecto nam eaque aliquid, repellendus enim ullam tempora.dolor sit amet consectetur adipisicing elit. Molestiae minus sapiente voluptates repellendus vel, magnam laborum aliquam quod fugiat quibusdam. Ullam eius error accusamus est commodi aut vero asperiores sequi. Lorem</p>
+    </section>
+  )
+}
+
+export default About
